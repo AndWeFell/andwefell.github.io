4469648bfd0eaca93d5dbe997cca64baa2130105
AndWeFell <andwefell@gmail.com>
2017-07-05 19:11:42 -0700

    Stray website

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ AFI Thesis Page
 
 ## Copyright and License
 
-Copyright 2016 Night Life Films
+Copyright 2017 Night Life Films
 
 Copyright 2013-2015 Iron Summit Media Strategies, LLC. Code released under the [Apache 2.0](https://github.com/IronSummitMedia/startbootstrap-modern-business/blob/gh-pages/LICENSE) license.
--- js/contact_me.js
+++ js/contact_me.js
@@ -50,7 +50,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong> Please email me directly at andwefell@gmail.com. Sorry for the inconvenience!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong> Please email me directly at strayafifilm@gmail.com. Sorry for the inconvenience!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
